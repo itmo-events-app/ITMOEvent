29aaca77e8b9efbf5815f716cdb198c01f5dbb6a
annyamd <miann9920@gmail.com>
2024-03-13 11:14:58 +0300

    Initial commit

---
+++ app/src/main/java/org/itmo/itmoevent/EventApplication.kt
@@ -0,0 +1,6 @@
+package org.itmo.itmoevent
+
+import android.app.Application
+
+class EventApplication : Application() {
+}
---
+++ app/src/main/java/org/itmo/itmoevent/view/MainActivity.kt
@@ -0,0 +1,7 @@
+package org.itmo.itmoevent.view
+
+import androidx.appcompat.app.AppCompatActivity
+import org.itmo.itmoevent.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+}
