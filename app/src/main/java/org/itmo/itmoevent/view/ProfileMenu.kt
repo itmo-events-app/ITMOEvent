2b3f7d8a3696b7be42b575e5a585f5318ec4a0c7
Daniil Fridkin <daniilfridkin@gmail.com>
2024-03-15 05:26:54 +0300

    Extract resources

--- app/src/main/java/org/itmo/itmoevent/view/ProfileMenu.kt
+++ app/src/main/java/org/itmo/itmoevent/view/ProfileMenu.kt
@@ -5,21 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.content.ContextCompat
-import androidx.fragment.app.FragmentManager
-import org.itmo.itmoevent.R
 import org.itmo.itmoevent.databinding.ProfileMenuBinding
 
 
 class ProfileMenu : Fragment() {
 
     private lateinit var binding: ProfileMenuBinding
-    private lateinit var fragmentManager: FragmentManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = ProfileMenuBinding.inflate(inflater)
         bind()
 
