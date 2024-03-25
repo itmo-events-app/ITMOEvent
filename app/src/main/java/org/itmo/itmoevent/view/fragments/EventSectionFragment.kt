bfea11cbeaed2d998efd4a4fb2f11eafdeca70c7
annyamd <miann9920@gmail.com>
2024-03-25 23:40:49 +0300

    Add bottom tab transitions to fragments impl

---
+++ app/src/main/java/org/itmo/itmoevent/view/fragments/EventSectionFragment.kt
@@ -0,0 +1,8 @@
+package org.itmo.itmoevent.view.fragments
+
+import androidx.fragment.app.Fragment
+import org.itmo.itmoevent.R
+
+
+class EventSectionFragment : Fragment(R.layout.fragment_event_section) {
+}
---
+++ app/src/main/java/org/itmo/itmoevent/view/fragments/TaskSectionFragment.kt
@@ -0,0 +1,9 @@
+package org.itmo.itmoevent.view.fragments
+
+import androidx.fragment.app.Fragment
+import org.itmo.itmoevent.R
+
+
+class TaskSectionFragment : Fragment(R.layout.fragment_task_section) {
+
+}
