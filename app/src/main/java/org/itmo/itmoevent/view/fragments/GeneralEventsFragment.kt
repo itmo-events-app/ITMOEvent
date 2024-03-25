cd90924b676e55095032195a660c81350b9897d4
annyamd <miann9920@gmail.com>
2024-03-25 23:45:46 +0300

    Add tab layout to EventSectionFragment

---
+++ app/src/main/java/org/itmo/itmoevent/view/fragments/GeneralEventsFragment.kt
@@ -0,0 +1,8 @@
+package org.itmo.itmoevent.view.fragments
+
+import androidx.fragment.app.Fragment
+import org.itmo.itmoevent.R
+
+class GeneralEventsFragment : Fragment(R.layout.fragment_general_events) {
+
+}
