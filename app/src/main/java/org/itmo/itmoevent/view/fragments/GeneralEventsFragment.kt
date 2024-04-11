be0d296c84fbbda93d5bb7981846a77af90dab70
annyamd <miann9920@gmail.com>
2024-04-11 11:37:28 +0300

    Impl privileges logic in MainEventsViewModel (hide content if sys pvlg not loaded, hide filter/rv if no such pvlg)

--- app/src/main/java/org/itmo/itmoevent/view/fragments/GeneralEventsFragment.kt
+++ app/src/main/java/org/itmo/itmoevent/view/fragments/GeneralEventsFragment.kt
@@ -7,6 +7,7 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
@@ -17,6 +18,8 @@ import org.itmo.itmoevent.databinding.FragmentGeneralEventsBinding
 import org.itmo.itmoevent.view.adapters.EventAdapter
 import org.itmo.itmoevent.viewmodel.EventItemViewModel
 import org.itmo.itmoevent.viewmodel.MainEventsViewModel
+import org.itmo.itmoevent.viewmodel.MainEventsViewModel.Function
+import org.itmo.itmoevent.viewmodel.MainEventsViewModel.Function.*
 import java.lang.IllegalStateException
 
 
@@ -28,8 +31,12 @@ class GeneralEventsFragment : Fragment(R.layout.fragment_general_events),
     private val model: MainEventsViewModel by viewModels {
         val application = requireActivity().application as? EventApplication
             ?: throw IllegalStateException("Application must be EventApplication implementation")
-        MainEventsViewModel.MainEventsViewModelFactory(application.eventRepository)
+        MainEventsViewModel.MainEventsViewModelFactory(
+            application.roleRepository,
+            application.eventRepository
+        )
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,31 +55,57 @@ class GeneralEventsFragment : Fragment(R.layout.fragment_general_events),
         viewBinding?.run {
             generalEventsRv.adapter = eventAdapter
             generalEventsRv.layoutManager = LinearLayoutManager(context)
-        }
 
-        model.eventsLiveData.observe(this.viewLifecycleOwner) { eventList ->
-            if (eventList == null) {
-                Toast.makeText(context, getString(R.string.no_found_message), Toast.LENGTH_LONG)
-                    .show()
-            } else {
-                eventAdapter.eventList = eventList
+            model.eventsLiveData.observe(this@GeneralEventsFragment.viewLifecycleOwner) { eventList ->
+                if (eventList == null) {
+                    Toast.makeText(
+                        context,
+                        getString(R.string.no_found_message),
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                } else {
+                    eventAdapter.eventList = eventList
+                }
+            }
+
+            model.isEventListLoading.observe(this@GeneralEventsFragment.viewLifecycleOwner) { isLoading ->
+                generalEventsRv.isVisible = !isLoading
+                genEventsProgressBar.root.isVisible = isLoading
+            }
+
+            model.disabledFunctionsLiveData.observe(this@GeneralEventsFragment.viewLifecycleOwner) { disabledFunctions ->
+                val funcViewMap = getFuncViewMap(this)
+                if (disabledFunctions == null) {
+                    showShortToast("Не удалось загрузить привилегии")
+                } else {
+                    disabledFunctions.forEach { func ->
+                        val viewToHide = funcViewMap[func]
+                        if (viewToHide != null) {
+                            viewToHide.isVisible = false
+                        }
+                    }
+                }
+            }
+
+            model.isPrivilegesLoading.observe(this@GeneralEventsFragment.viewLifecycleOwner) { isLoading ->
+                generalEventsRv.isVisible = !isLoading
             }
         }
-
-        model.isEventListLoading.observe(this.viewLifecycleOwner) { isLoading ->
-            var progressBarVisibility = GONE
-            var recyclerVisibility = VISIBLE
-            if (isLoading) {
-                progressBarVisibility = VISIBLE
-                recyclerVisibility = GONE
-            }
-            viewBinding?.run {
-                genEventsProgressBar.progressBar.visibility = progressBarVisibility
-                generalEventsRv.visibility = recyclerVisibility
-            }
-        }
-
     }
+
+    private fun showShortToast(text: String) {
+        Toast.makeText(
+            context,
+            text,
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    private fun getFuncViewMap(binding: FragmentGeneralEventsBinding): Map<Function, View> = mapOf(
+        SEE_EVENTS to binding.generalEventsRv,
+        FILTER_EVENTS to binding.genEventsFilterCard
+    )
 
     override fun onEventClicked(eventId: Int) {
         eventItemViewModel.selectEventItem(eventId)
--- app/src/main/java/org/itmo/itmoevent/viewmodel/MainEventsViewModel.kt
+++ app/src/main/java/org/itmo/itmoevent/viewmodel/MainEventsViewModel.kt
@@ -5,18 +5,48 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
+import androidx.lifecycle.switchMap
 import org.itmo.itmoevent.model.data.entity.EventShort
+import org.itmo.itmoevent.model.data.entity.enums.PrivilegeName
+import org.itmo.itmoevent.model.data.entity.enums.PrivilegeName.*
 import org.itmo.itmoevent.model.repository.EventRepository
+import org.itmo.itmoevent.model.repository.RoleRepository
 
 
-class MainEventsViewModel(private val eventsRepository: EventRepository) : ViewModel() {
-
-    val eventsLiveData: LiveData<List<EventShort>?> = liveData {
-        val loaded = loadEvents()
-        emit(loaded)
-    }
+class MainEventsViewModel(
+    private val roleRepository: RoleRepository,
+    private val eventsRepository: EventRepository
+) : ViewModel() {
 
     val isEventListLoading = MutableLiveData<Boolean>()
+    val isPrivilegesLoading = MutableLiveData<Boolean>()
+
+
+    val disabledFunctionsLiveData: LiveData<List<Function>?> = liveData {
+        isPrivilegesLoading.value = true
+        val privileges = roleRepository.getSystemPrivileges()
+        if (privileges == null) {
+            emit(null)
+        } else {
+            val disabledFunctions = Function.values().filter { func ->
+                !privileges
+                    .map { it.name }
+                    .contains(mapFuncToPrivilegeName(func))
+            }
+            emit(disabledFunctions)
+            isPrivilegesLoading.value = false
+        }
+    }
+
+    val eventsLiveData: LiveData<List<EventShort>?> =
+        disabledFunctionsLiveData.switchMap { disabled ->
+            liveData {
+                if (disabled != null && !disabled.contains(Function.SEE_EVENTS)) {
+                    val loaded = loadEvents()
+                    emit(loaded)
+                }
+            }
+        }
 
     private suspend fun loadEvents(): List<EventShort>? {
         isEventListLoading.value = true
@@ -25,16 +55,28 @@ class MainEventsViewModel(private val eventsRepository: EventRepository) : ViewM
         return loaded
     }
 
+    enum class Function {
+        SEE_EVENTS,
+        FILTER_EVENTS
+    }
+
+    private fun mapFuncToPrivilegeName(func: Function): PrivilegeName =
+        when (func) {
+            Function.SEE_EVENTS -> VIEW_ALL_EVENTS
+            Function.FILTER_EVENTS -> SEARCH_EVENTS_AND_ACTIVITIES
+        }
+
+
     class MainEventsViewModelFactory(
-        private val repository: EventRepository
+        private val roleRepository: RoleRepository,
+        private val eventRepository: EventRepository
     ) :
         ViewModelProvider.Factory {
         override fun <T : ViewModel> create(modelClass: Class<T>): T {
             if (modelClass.isAssignableFrom(MainEventsViewModel::class.java)) {
-                return MainEventsViewModel(repository) as T
+                return MainEventsViewModel(roleRepository, eventRepository) as T
             }
             throw IllegalArgumentException("Unknown ViewModel class")
         }
     }
-
 }
