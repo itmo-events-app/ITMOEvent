596da59dd831f1180575c7f87952337576d5be4c
annyamd <miann9920@gmail.com>
2024-04-17 22:18:29 +0300

    Refactor: short ActivityFragment

--- app/src/main/java/org/itmo/itmoevent/view/fragments/ActivityFragment.kt
+++ app/src/main/java/org/itmo/itmoevent/view/fragments/ActivityFragment.kt
@@ -4,47 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.LiveData
-import com.google.android.material.tabs.TabLayout
-import org.itmo.itmoevent.EventApplication
 import org.itmo.itmoevent.R
 import org.itmo.itmoevent.databinding.FragmentActivBinding
 import org.itmo.itmoevent.model.data.entity.EventsActivity
 import org.itmo.itmoevent.model.data.entity.Place
 import org.itmo.itmoevent.view.util.DisplayDateFormats
-import org.itmo.itmoevent.viewmodel.ContentItemLiveDataProvider
 import org.itmo.itmoevent.viewmodel.EventActivityViewModel
 import org.itmo.itmoevent.viewmodel.MainViewModel
 import java.time.format.DateTimeFormatter
 
-class ActivityFragment : Fragment(R.layout.fragment_activ) {
+class ActivityFragment : BaseFragment<FragmentActivBinding>() {
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentActivBinding
+        get() = { inflater, container, attach ->
+            FragmentActivBinding.inflate(inflater, container, attach)
+        }
 
-    private var viewBinding: FragmentActivBinding? = null
     private var activityId: Int? = null
-    private var model: EventActivityViewModel? = null
     private val mainViewModel: MainViewModel by activityViewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        viewBinding = FragmentActivBinding.inflate(inflater, container, false)
-        return viewBinding?.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun setup(view: View, savedInstanceState: Bundle?) {
         val activityId = requireArguments().getInt(ACTIVITY_ID_ARG)
         this.activityId = activityId
 
         val model: EventActivityViewModel by viewModels {
-            val application = requireActivity().application as? EventApplication
-                ?: throw IllegalStateException("Application must be EventApplication implementation")
             EventActivityViewModel.ActivityViewModelModelFactory(
                 activityId,
                 application.eventActivityRepository,
@@ -53,137 +38,68 @@ class ActivityFragment : Fragment(R.layout.fragment_activ) {
             )
         }
 
-        viewBinding?.run {
-
-            handleContentItemViewByLiveData<EventsActivity>(
+        viewBinding.run {
+            handleContentItemViewByLiveData(
                 model.activityInfoLiveData,
                 activityContent,
-                activityProgressBarMain.root
-            ) { event ->
-                activityInfo.run {
-                    val formatter =
-                        DateTimeFormatter.ofPattern(DisplayDateFormats.DATE_EVENT_FULL)
-                    event.run {
-                        eventTitle.text = title
-                        eventShortDesc.text = shortDesc
-                        eventDescLong.text = longDesc
-                        eventChipStatus.text = status
-                        eventChipTime.text = startDate.format(formatter)
-                        eventDetailsTimeHold.text =
-                            getString(
-                                R.string.event_duration,
-                                startDate.format(formatter),
-                                endDate.format(formatter)
-                            )
-                        eventDetailsTimeRegister.text =
-                            getString(
-                                R.string.event_duration,
-                                regStart.format(formatter),
-                                regEnd.format(formatter)
-                            )
-                        eventDetailsTimePrepare.text =
-                            getString(
-                                R.string.event_duration,
-                                preparingStart.format(formatter),
-                                preparingEnd.format(formatter)
-                            )
-                        eventDetailsAge.text = participantAgeLowest.toString()
-                        eventDetailsParticipantsMax.text = participantLimit.toString()
-                    }
-                }
-            }
+                activityProgressBarMain.root,
+                bindContent = ::bindActivityInfo
+            )
 
-            handleContentItemViewByLiveData<Place>(
+            handleContentItemViewByLiveData(
                 model.placeLiveData,
-                activityInfo.eventPlaceCard.root
-            ) { place ->
-                activityInfo.run {
-                    eventChipPlace.text = place.name
-                    eventPlaceCard.placeItemTitle.text = place.name
-                    eventPlaceCard.placeItemDesc.text = place.description
-                    eventPlaceCard.placeItemFormat.text = place.format
-
-                    eventPlaceCard.root.setOnClickListener {
-                        mainViewModel.selectPlace(place.id)
-                    }
-                }
-            }
+                activityInfo.eventPlaceCard.root,
+                bindContent = ::bindPlace
+            )
         }
-
     }
 
-    private fun <T> handleContentItemViewByLiveData(
-        livedata: LiveData<ContentItemLiveDataProvider.ContentItemUIState>,
-        contentView: View,
-        progressBar: View? = null,
-        needToShow: Boolean = true,
-        ifDisabled: (() -> Unit)? = null,
-        bindContent: (T) -> Unit
-    ) {
-        livedata.observe(this.viewLifecycleOwner) { state ->
-            when (state) {
-                is ContentItemLiveDataProvider.ContentItemUIState.Success<*> -> {
-                    val content = state.content!! as T
-                    bindContent(content)
-                    if (needToShow) {
-                        show(contentView)
-                        progressBar?.let {
-                            hide(progressBar)
-                        }
-                    }
-                }
+    private fun bindPlace(place: Place) {
+        viewBinding.activityInfo.run {
+            eventChipPlace.text = place.name
+            eventPlaceCard.placeItemTitle.text = place.name
+            eventPlaceCard.placeItemDesc.text = place.description
+            eventPlaceCard.placeItemFormat.text = place.format
 
-                is ContentItemLiveDataProvider.ContentItemUIState.Error -> {
-                    if (needToShow) {
-                        show(contentView)
-                        progressBar?.let {
-                            hide(progressBar)
-                        }
-                    }
-                    state.errorMessage?.let {
-                        showShortToast(it)
-                    }
-                }
-
-                is ContentItemLiveDataProvider.ContentItemUIState.Disabled -> {
-                    if (needToShow) {
-                        hide(contentView)
-                    }
-                    ifDisabled?.invoke()
-                    showShortToast("Blocked")
-                }
-
-                is ContentItemLiveDataProvider.ContentItemUIState.Loading -> {
-                    if (needToShow) {
-                        hide(contentView)
-                        progressBar?.let {
-                            show(progressBar)
-                        }
-                    }
-                }
-
-                is ContentItemLiveDataProvider.ContentItemUIState.Start -> {
-                }
+            eventPlaceCard.root.setOnClickListener {
+                mainViewModel.selectPlace(place.id)
             }
         }
     }
 
-    private fun show(view: View?) {
-        view?.visibility = View.VISIBLE
+    private fun bindActivityInfo(activity: EventsActivity) {
+        viewBinding.activityInfo.run {
+            val formatter =
+                DateTimeFormatter.ofPattern(DisplayDateFormats.DATE_EVENT_FULL)
+            activity.run {
+                eventTitle.text = title
+                eventShortDesc.text = shortDesc
+                eventDescLong.text = longDesc
+                eventChipStatus.text = status
+                eventChipTime.text = startDate.format(formatter)
+                eventDetailsTimeHold.text =
+                    getString(
+                        R.string.event_duration,
+                        startDate.format(formatter),
+                        endDate.format(formatter)
+                    )
+                eventDetailsTimeRegister.text =
+                    getString(
+                        R.string.event_duration,
+                        regStart.format(formatter),
+                        regEnd.format(formatter)
+                    )
+                eventDetailsTimePrepare.text =
+                    getString(
+                        R.string.event_duration,
+                        preparingStart.format(formatter),
+                        preparingEnd.format(formatter)
+                    )
+                eventDetailsAge.text = participantAgeLowest.toString()
+                eventDetailsParticipantsMax.text = participantLimit.toString()
+            }
+        }
     }
-
-    private fun hide(view: View?) {
-        view?.visibility = TabLayout.GONE
-    }
-
-    private fun showShortToast(text: String) {
-        Toast.makeText(
-            context,
-            text,
-            Toast.LENGTH_LONG
-        ).show()
-    }
-
 
     companion object {
         const val ACTIVITY_ID_ARG: String = "activityId"
