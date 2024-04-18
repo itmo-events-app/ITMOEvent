68d009721e1e16eef2c2aef126903e975a7fd19d
annyamd <miann9920@gmail.com>
2024-04-18 14:52:10 +0300

    Refactor UserEventsFragment

--- app/src/main/java/org/itmo/itmoevent/view/fragments/UserEventsFragment.kt
+++ app/src/main/java/org/itmo/itmoevent/view/fragments/UserEventsFragment.kt
@@ -3,192 +3,97 @@ package org.itmo.itmoevent.view.fragments
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.GONE
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.core.view.isVisible
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
-import org.itmo.itmoevent.EventApplication
-import org.itmo.itmoevent.R
 import org.itmo.itmoevent.databinding.FragmentUserEventsBinding
 import org.itmo.itmoevent.model.data.entity.EventRequest
 import org.itmo.itmoevent.model.data.entity.EventShort
 import org.itmo.itmoevent.view.adapters.EventRequestAdapter
 import org.itmo.itmoevent.view.adapters.EventAdapter
-import org.itmo.itmoevent.viewmodel.ContentItemLiveDataProvider
+import org.itmo.itmoevent.view.fragments.base.BaseFragment
 import org.itmo.itmoevent.viewmodel.MainViewModel
 import org.itmo.itmoevent.viewmodel.UserEventsViewModel
-import java.lang.IllegalStateException
 
 
-class UserEventsFragment : Fragment(R.layout.fragment_user_events) {
-    private var viewBinding: FragmentUserEventsBinding? = null
+class UserEventsFragment : BaseFragment<FragmentUserEventsBinding>() {
 
-//    private val model: UserEventsViewModel by viewModels {
-//        val application = requireActivity().application as? EventApplication
-//            ?: throw IllegalStateException("Application must be EventApplication implementation")
-//        UserEventsViewModel.UserEventsViewModelFactory(
-//            application.eventRequestRepository,
-//            application.roleRepository,
-//            application.eventRepository
-//        )
-//    }
-
+    private var _model: UserEventsViewModel? = null
+    private val model: UserEventsViewModel
+        get() = _model as UserEventsViewModel
     private val eventItemViewModel: MainViewModel by activityViewModels()
 
+    private var requestAdapter: EventRequestAdapter? = null
+    private var eventAdapter: EventAdapter? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        viewBinding = FragmentUserEventsBinding.inflate(inflater, container, false)
-        return viewBinding?.root
-    }
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentUserEventsBinding
+        get() = { inflater, container, attach ->
+            FragmentUserEventsBinding.inflate(inflater, container, attach)
+        }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
+    override fun setup(view: View, savedInstanceState: Bundle?) {
         val model: UserEventsViewModel by viewModels {
-            val application = requireActivity().application as? EventApplication
-                ?: throw IllegalStateException("Application must be EventApplication implementation")
             UserEventsViewModel.UserEventsViewModelFactory(
                 application.eventRequestRepository,
                 application.roleRepository,
                 application.eventRepository
             )
         }
+        _model = model
 
-        val requestsAdapter = EventRequestAdapter()
-        val eventsAdapter = EventAdapter(::onEventClicked)
+        setupRecyclerViews()
+        registerViewListeners()
+        observeLiveData()
+    }
 
-        viewBinding?.run {
+    private fun setupRecyclerViews() {
+        requestAdapter = requestAdapter ?: EventRequestAdapter()
+        eventAdapter = eventAdapter ?: EventAdapter(::onEventClicked)
+
+        viewBinding.run {
             userEventsRequestsRv.layoutManager = LinearLayoutManager(context)
-            userEventsRequestsRv.adapter = requestsAdapter
+            userEventsRequestsRv.adapter = requestAdapter
 
+            userEventsOrganizedRv.layoutManager = LinearLayoutManager(context)
+            userEventsOrganizedRv.adapter = eventAdapter
+        }
+    }
+
+    private fun registerViewListeners() {
+        viewBinding.run {
             userEventsRoleEdit.setOnItemClickListener { _, _, position, _ ->
                 model.roleNameIndex.value = position
             }
-            userEventsProgressBarRoleEvents.progressBar.visibility = GONE
+        }
+    }
 
-            userEventsOrganizedRv.layoutManager = LinearLayoutManager(context)
-            userEventsOrganizedRv.adapter = eventsAdapter
-            eventsAdapter.eventList = listOf()
-
-
-            //            main item, has no privileges
+    private fun observeLiveData() {
+        viewBinding.run {
             handleContentItemViewByLiveData<List<String>?>(
-                model.roleListLiveData,
-                userEventsGroupRoleEvents,
-                userEventsProgressBarMain.root
+                model.roleListLiveData, userEventsGroupRoleEvents, userEventsProgressBarMain.root
             ) { roles ->
-                viewBinding?.run {
-                    (viewBinding?.userEventsRoleEdit as? MaterialAutoCompleteTextView)?.setSimpleItems(
-                        roles?.toTypedArray() ?: emptyArray()
-                    )
-                }
+                (viewBinding.userEventsRoleEdit as? MaterialAutoCompleteTextView)?.setSimpleItems(
+                    roles?.toTypedArray() ?: emptyArray()
+                )
             }
 
             handleContentItemViewByLiveData<List<EventRequest>?>(
-                model.eventRequestsLiveData,
-                userEventsGroupRequests
+                model.eventRequestsLiveData, userEventsGroupRequests
             ) { eventRequests ->
-                viewBinding?.run {
-                    requestsAdapter.eventRequests = eventRequests ?: emptyList()
-                }
+                requestAdapter?.eventRequests = eventRequests ?: emptyList()
             }
 
             handleContentItemViewByLiveData<List<EventShort>?>(
-                model.roleEventListLiveData,
-                userEventsOrganizedRv,
+                model.roleEventListLiveData, userEventsOrganizedRv,
                 userEventsProgressBarRoleEvents.progressBar,
             ) { eventList ->
-                viewBinding?.run {
-                    eventsAdapter.eventList = eventList ?: emptyList()
-                }
+                eventAdapter?.eventList = eventList ?: emptyList()
             }
 
         }
     }
-
-    private fun <T> handleContentItemViewByLiveData(
-        livedata: LiveData<ContentItemLiveDataProvider.ContentItemUIState>,
-        contentView: View,
-        progressBar: View? = null,
-        needToShow: Boolean = true,
-        ifDisabled: (() -> Unit)? = null,
-        bindContent: (T) -> Unit
-    ) {
-        livedata.observe(this.viewLifecycleOwner) { state ->
-            when (state) {
-                is ContentItemLiveDataProvider.ContentItemUIState.Success<*> -> {
-                    val content = state.content!! as T
-                    bindContent(content)
-                    if (needToShow) {
-                        show(contentView)
-                        progressBar?.let {
-                            hide(progressBar)
-                        }
-                    }
-                }
-
-                is ContentItemLiveDataProvider.ContentItemUIState.Error -> {
-                    if (needToShow) {
-                        show(contentView)
-                        progressBar?.let {
-                            hide(progressBar)
-                        }
-                    }
-                    state.errorMessage?.let {
-                        showShortToast(it)
-                    }
-                }
-
-                is ContentItemLiveDataProvider.ContentItemUIState.Disabled -> {
-                    if (needToShow) {
-                        hide(contentView)
-                    }
-                    ifDisabled?.invoke()
-                    showShortToast("Blocked")
-                }
-
-                is ContentItemLiveDataProvider.ContentItemUIState.Loading -> {
-                    if (needToShow) {
-                        hide(contentView)
-                        progressBar?.let {
-                            show(progressBar)
-                        }
-                    }
-                }
-
-                is ContentItemLiveDataProvider.ContentItemUIState.Start -> {
-                }
-            }
-        }
-    }
-
-
-    private fun show(view: View?) {
-        view?.isVisible = true
-    }
-
-    private fun hide(view: View?) {
-        view?.isVisible = false
-    }
-
-    private fun showShortToast(text: String) {
-        Toast.makeText(
-            context,
-            text,
-            Toast.LENGTH_LONG
-        ).show()
-    }
-
 
     private fun onEventClicked(eventId: Int) {
         eventItemViewModel.selectEventItem(eventId)
