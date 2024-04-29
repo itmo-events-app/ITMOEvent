89a3dd8e7eb527f6b8e99ae1e97af84ab1a625cc
annyamd <miann9920@gmail.com>
2024-04-29 16:53:17 +0300

    Fix: delete extra in network

--- app/src/main/java/org/itmo/itmoevent/model/data/entity/EventDetails.kt
+++
@@ -1,9 +0,0 @@
-package org.itmo.itmoevent.model.data.entity
-
-data class EventDetails(
-    val event: Event,
-    val activities: List<EventShort>,
-    val orgRolesMap: Map<String, List<UserRole>>,
-    val place: Place,
-    val participants: List<Participant>
-)
